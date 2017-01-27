327c54808406405a08877540d9de6e852a609ab6
seanrobinson1114 <seanrobinson1114@gmail.com>
2017-01-27 14:19:37 -0600

    model for trip table

--- freetimey/trips/models.py
+++ freetimey/trips/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+class Trip(models.Model):
+    trip_name = models.charField(max_length=63)
+    picture_name = models.charField(max_length=63)
+    blob = ImageField(
+    
+
