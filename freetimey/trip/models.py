f92500dbc9921651c7c14aa87ecf6730f1f204b6
sean robinson <seanrobinson1114@gmail.com>
2017-01-27 18:19:10 -0600

    changed directory problem

--- freetimey/trip/models.py
+++ freetimey/trip/models.py
@@ -1,12 +1,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.db.models.fields.files import ImageField
 
-# Create your models here.
 class Trip(models.Model):
-    trip_name = models.charField(max_length=63)
-    picture_name = models.charField(max_length=63)
-    blob = ImageField(
-    
-
+    name = models.CharField(max_length=63, unique=True, help_text='Should be same as folder name')
+    date = models.DateField()
+    state = models.CharField(max_length=63)
+    area = models.CharField(max_length=63, help_text='e.g. Little Bear, Denali, Sante Fe, West Coast, ...')
+    type = models.CharField(max_length=63, help_text='e.g. Mountaineering, Hiking, Family Vacation, Climbing, ...')
