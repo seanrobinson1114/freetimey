59602651c7a47e6c029ed8fb78642a610871697d
sean robinson <seanrobinson1114@gmail.com>
2017-02-04 21:36:19 -0600

    figuring out how to best store object

--- freetimey/trip/views.py
+++ freetimey/trip/views.py
@@ -4,11 +4,29 @@ from django.template import Context, loader
 
 from .models import Trip
 
+class Object(object):
+    pass
+
 # Create your views here.
 def homepage(request):
     trip_list = Trip.objects.all()
-    # output = ", ".join([trip.name for trip in trip_list])
+    # trip = Trip.objects.get(pk="Denali July 2015")
+    # image_list = trip.images.all()
+    trip_images = Object()
+    trip_images.trips = trip_list
+    trip_images.images = [None] * len(trip_list)
+    for index, trip in enumerate(trip_list):
+        # trip_images.trip = trip
+        trip_images.images[index] = getTripImages(trip)
     template = loader.get_template('index.html')
-    context = Context({'trip_list': trip_list})
+    # context = Context({'trip_list': trip_list, 'image_list': image_list, 'trip_images': trip_images})
+    context = Context({'trip_list': trip_list, 'trip_images': trip_images, 'number_of_trips': range(len(trip_list))})
     output = template.render(context)
     return HttpResponse(output)
+
+def getTripImages(trip_name):
+    print "parameter for function: "
+    print trip_name
+    trip = Trip.objects.get(pk=trip_name)
+    image_list = trip.images.all()
+    return image_list
